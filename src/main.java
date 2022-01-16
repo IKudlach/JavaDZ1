00cc3970f745f6f8fd0c697b70bef4fb1bcf554e
Ikudlach <igorkudlach@gmail.com>
2022-01-16 19:05:27 +0300

    Creat Java Prog

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int amount = 1_000_000;
+        int bonus = amount / 20;
+        System.out.println("Вам начисленно бонусных миль");
+        System.out.println(bonus);
+    }
+}
